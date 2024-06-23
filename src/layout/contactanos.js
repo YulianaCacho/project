3e5d2ddf47687d24f05cc0267a74f8daf3358420
YulianaCacho <166524112+YulianaCacho@users.noreply.github.com>
2024-06-23 23:11:12 +0000

    Modificando Contactanos

--- src/layout/contactanos.js
+++ src/layout/contactanos.js
@@ -1,56 +1,59 @@
+import React from 'react';
+import '/workspaces/project/src/Contactanos.css'; // Asegúrate de crear e importar un archivo CSS para estilos personalizados
+
 const Contactanos = () => {
-   return (
-       <aside style={{ backgroundColor: '#b3e5fc', padding: '20px' }}>
-           <br />
-           <center><h2>Contactanos</h2></center>
-           <br />
-           <div className="container">
-               <div className="row">
-                   <div className="col-md-6">
-                       <form>
-                           <div className="mb-3">
-                               <label htmlFor="formGroupExampleInput" className="form-label">
-                                   <i data-feather="user"></i> Nombre y apellidos:
-                               </label>
-                               <input type="text" required className="form-control" id="formGroupExampleInput" placeholder="Diego Bohorquez Novoa" />
-                           </div>
-                           <br />
-                           <div className="mb-3">
-                               <label htmlFor="formGroupExampleInput2" className="form-label">
-                                   <i data-feather="mail"></i> Correo Electrónico:
-                               </label>
-                               <input type="email" required className="form-control" id="formGroupExampleInput2" placeholder="liliana_24@hotmail.com" />
-                           </div>
-                           <br />
-                           <div className="mb-3">
-                               <label htmlFor="formGroupExampleInput3" className="form-label">
-                                   <i data-feather="users"></i> Mensaje:
-                               </label>
-                               <textarea required className="form-control" id="formGroupExampleInput3" placeholder="Escribe tu mensaje aquí" />
-                           </div>
-                           <br />
-                           <center>
-                               <button className="btn btn-primary">
-                                   <i data-feather="log-in"></i> Enviar
-                               </button>
-                           </center>
-                           <br />
-                       </form>
-                   </div>
-                   <div className="col-md-6">
-                       <center><h3>Redes Sociales</h3></center>
-                       <br />
-                       <ul className="list-unstyled">
-                           <li><i data-feather="facebook"></i> Facebook</li>
-                           <li><i data-feather="twitter"></i> Twitter</li>
-                           <li><i data-feather="instagram"></i> Instagram</li>
-                           <li><i data-feather="linkedin"></i> LinkedIn</li>
-                       </ul>
-                   </div>
-               </div>
-           </div>
-       </aside>
-   );
+    return (
+        <aside style={{ backgroundColor: '#b3e5fc', padding: '20px' }}>
+            <br />
+            <center><h2>Contactanos</h2></center>
+            <br />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="formGroupExampleInput" className="form-label">
+                                    <i data-feather="user"></i> Nombre y apellidos:
+                                </label>
+                                <input type="text" required className="form-control" id="formGroupExampleInput" placeholder="Diego Bohorquez Novoa" />
+                            </div>
+                            <br />
+                            <div className="mb-3">
+                                <label htmlFor="formGroupExampleInput2" className="form-label">
+                                    <i data-feather="mail"></i> Correo Electrónico:
+                                </label>
+                                <input type="email" required className="form-control" id="formGroupExampleInput2" placeholder="liliana_24@hotmail.com" />
+                            </div>
+                            <br />
+                            <div className="mb-3">
+                                <label htmlFor="formGroupExampleInput3" className="form-label">
+                                    <i data-feather="users"></i> Mensaje:
+                                </label>
+                                <textarea required className="form-control" id="formGroupExampleInput3" placeholder="Escribe tu mensaje aquí"></textarea>
+                            </div>
+                            <br />
+                            <center>
+                                <button className="btn btn-primary">
+                                    <i></i> Enviar
+                                </button>
+                            </center>
+                            <br />
+                        </form>
+                    </div>
+                    <div className="col-md-6">
+                        <center><h3 className="redes-title">Redes Sociales</h3></center>
+                        <br />
+                        <ul className="list-unstyled social-list">
+                            <li><i data-feather="facebook"></i> Facebook</li>
+                            <li><i data-feather="twitter"></i> Twitter</li>
+                            <li><i data-feather="instagram"></i> Instagram</li>
+                            <li><i data-feather="linkedin"></i> LinkedIn</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    );
 };
 
 export default Contactanos;

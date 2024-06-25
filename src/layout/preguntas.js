fc3ea6eccf9b6357e8c60d1b25e08011410cca61
YulianaCacho <166524112+YulianaCacho@users.noreply.github.com>
2024-06-25 02:29:04 +0000

    Agregando las preguntas y respuestas

--- src/layout/preguntas.js
+++ src/layout/preguntas.js
@@ -27,12 +27,14 @@ const Preguntas = () => {
                         </div>
                         {activeIndex === 0 && 
                            <div className="card-body respuesta">
-                             <br></br>
-                              <center>
-                              Los procesadores Intel y AMD tienen diferencias en arquitectura, rendimiento y precios. Intel suele ofrecer un mejor rendimiento por núcleo, 
-                              lo que es beneficioso para aplicaciones de un solo hilo, mientras que AMD ofrece más núcleos y hilos a un precio más competitivo, 
-                              lo cual es ideal para multitarea y aplicaciones multihilo.
-                              </center>
+                             <center>
+                              <br></br>
+                              Los procesadores Intel y AMD tienen diferencias en arquitectura, rendimiento y precios. Intel
+                              suele ofrecer un mejor rendimiento por núcleo, lo que es beneficioso para aplicaciones de un 
+                              solo hilo, mientras que AMD ofrece más núcleos y hilos a un precio más competitivo, lo cual es 
+                              ideal para multitarea y aplicaciones multihilo.
+                              <p></p>
+                             </center>
                             </div>}
                     </div>
                     <div
@@ -40,18 +42,38 @@ const Preguntas = () => {
                         onClick={() => toggleAnswer(1)}
                     >
                         <div className="card-body">
-                            <i className="bi bi-chevron-right"></i> Pregunta 2
+                            <i className="bi bi-chevron-right"></i>
+                            <strong>¿ESTE PROCESADOR ES COMPATIBLE CON MI SISTEMA OPERATIVO</strong>
                         </div>
-                        {activeIndex === 1 && <div className="card-body respuesta">Respuesta 2</div>}
+                        {activeIndex === 1 && 
+                        <div className="card-body respuesta">
+                            <center>
+                              <br></br>
+                              La mayoría de los procesadores modernos son compatibles con sistemas operativos recientes como 
+                              Windows 10 y 11, macOS, y varias distribuciones de Linux. Consulta las especificaciones del 
+                              procesador para asegurarte.
+                              <p></p>
+                             </center>
+                        </div>}
                     </div>
                     <div
                         className={`card btn-outline-primary mt-3 ${activeIndex === 2 ? 'active' : ''}`}
                         onClick={() => toggleAnswer(2)}
                     >
                         <div className="card-body">
-                            <i className="bi bi-chevron-right"></i> Pregunta 3
+                            <i className="bi bi-chevron-right"></i> 
+                            <strong>¿CUÁL ES LA DIFERENCIA ENTRE UNA TARJETA GRÁFICA DE GAMA ALTA Y UNA DE GAMA MEDIA?</strong>
                         </div>
-                        {activeIndex === 2 && <div className="card-body respuesta">Respuesta 3</div>}
+                        {activeIndex === 2 && 
+                        <div className="card-body respuesta">
+                           <center>
+                              <br></br>
+                              Las tarjetas gráficas de gama alta ofrecen un rendimiento superior, más VRAM, y características avanzadas 
+                              como ray tracing en tiempo real y mejor soporte para resoluciones 4K. Las de gama media ofrecen un buen 
+                              rendimiento a un precio más accesible, ideales para juegos en 1080p o 1440p.
+                              <p></p>
+                            </center>
+                        </div>}
                     </div>
                 </div>
                 <div className="col-md-6">
@@ -60,27 +82,58 @@ const Preguntas = () => {
                         onClick={() => toggleAnswer(3)}
                     >
                         <div className="card-body">
-                            <i className="bi bi-chevron-right"></i> Pregunta 4
+                            <i className="bi bi-chevron-right"></i> 
+                            <strong>¿QUÉ PROCESADOR ES MEJOR PARA JUEGOS?</strong>
                         </div>
-                        {activeIndex === 3 && <div className="card-body respuesta">Respuesta 4</div>}
+                        {activeIndex === 3 && 
+                        <div className="card-body respuesta">
+                           <center>
+                              <br></br>
+                              Los procesadores Intel Core i5 e i7, así como los AMD Ryzen 5 y Ryzen 7, son 
+                              excelentes opciones para juegos debido a su alto rendimiento por núcleo y buen 
+                              equilibrio entre núcleos e hilos.
+                              <p></p>
+                            </center>
+                        </div>}
                     </div>
                     <div
                         className={`card btn-outline-primary mt-3 ${activeIndex === 4 ? 'active' : ''}`}
                         onClick={() => toggleAnswer(4)}
                     >
                         <div className="card-body">
-                            <i className="bi bi-chevron-right"></i> Pregunta 5
+                            <i className="bi bi-chevron-right"></i> 
+                            <strong>¿QUÉ TRAJETA GRÁFICA NECESITO PARA EDICIÓN DE VIDEO PROFESIONAL</strong>
                         </div>
-                        {activeIndex === 4 && <div className="card-body respuesta">Respuesta 5</div>}
+                        {activeIndex === 4 && 
+                        <div className="card-body respuesta">
+                            <center>
+                              <br></br>
+                              Los procesadores Intel Core i5 e i7, así como los AMD Ryzen 5 y Ryzen 7, son 
+                              excelentes opciones para juegos debido a su alto rendimiento por núcleo y buen 
+                              equilibrio entre núcleos e hilos.
+                              <p></p>
+                            </center>
+                        </div>}
                     </div>
                     <div
                         className={`card btn-outline-primary mt-3 ${activeIndex === 5 ? 'active' : ''}`}
                         onClick={() => toggleAnswer(5)}
                     >
                         <div className="card-body">
-                            <i className="bi bi-chevron-right"></i> Pregunta 6
+                            <i className="bi bi-chevron-right"></i> 
+                            <strong>¿CUÁL ES LA DIFERENCIA ENTRE LOS PRODUCTOS NUEVOS, REACONDICIONADO Y USADOS?</strong>
                         </div>
-                        {activeIndex === 5 && <div className="card-body respuesta">Respuesta 6</div>}
+                        {activeIndex === 5 && 
+                        <div className="card-body respuesta">
+                            <center>
+                              <br></br>
+                              Los productos nuevos son completamente nuevos y vienen con la garantía completa del fabricante.
+                              Los productos reacondicionados han sido restaurados a su estado original y probados para asegurarse 
+                              de que funcionen correctamente, generalmente con una garantía limitada. Los productos usados pueden
+                               mostrar signos de uso y tener una garantía limitada o no tenerla.
+                              <p></p>
+                            </center>
+                        </div>}
                     </div>
                 </div>
             </div>
